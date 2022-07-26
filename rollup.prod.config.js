e3174417c8791edfcda03abc85b6de9686ac27de
aquarius-wing <aquarius_wing@icloud.com>
2022-07-26 16:09:06 +0800

    info(app): 改为rollup作为项目构建的工具

---
+++ rollup.prod.config.js
@@ -0,0 +1,11 @@
+import typescript from '@rollup/plugin-typescript';
+export default {
+    input: 'src/main.ts',
+    output: {
+        file: 'bundle.js',
+        format: 'cjs'
+    },
+    plugins: [typescript({
+        tsconfig: 'tsconfig.prod.json'
+    })]
+};
